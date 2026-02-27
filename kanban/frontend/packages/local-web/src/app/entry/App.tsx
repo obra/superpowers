0b205fc6b382bf0561007ed409f292f0fff52e3f
Jingxia Xing <touchskyer@jxxingWorkMacStudio.local>
2026-02-27 17:48:58 +0800

    chore: remove deprecated kanban/ directory (renamed to crew/)

--- kanban/frontend/packages/local-web/src/app/entry/App.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider } from '@tanstack/react-router'
-import { router } from '@web/app/router/index'
-
-function App() {
-  return <RouterProvider router={router} />
-}
-
-export default App
